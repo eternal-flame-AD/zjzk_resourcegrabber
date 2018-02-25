1895afe702b2cfa25b8c3d7c2ee14a40d579d2e4
_eternal_flame <35571479+eternal-flame-AD@users.noreply.github.com>
2018-02-25 12:11:16 +0000

    Update config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-server_path=""
-platform_name=""
-version=""
-fc_plist_path=""
-download_dir="./assets/"
-thread=10
+server_path="dlres.xiimoon.com"
+platform_name="Android"
+#platform_name="IOS"
+version="1.3.0_elite"
+fc_plist_path="./plist.txt"
+download_dir="./assets-ios/"
+thread=20
